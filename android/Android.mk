a96c501e99bc7de068540225102854994e30cad1
Jean-Baptiste Queru <jbq@google.com>
2010-09-26 10:06:46 -0700

    Mark ip-up-vpn as optional
    Change-Id: If584a830f59f259dd1206bd88df59463c850cb70

--- android/Android.mk
+++ android/Android.mk
@@ -21,5 +21,6 @@ LOCAL_SRC_FILES := ip-up-vpn.c
 LOCAL_SHARED_LIBRARIES := libcutils
 LOCAL_MODULE := ip-up-vpn
 LOCAL_MODULE_PATH := $(TARGET_OUT_ETC)/ppp
+LOCAL_MODULE_TAGS := optional
 
 include $(BUILD_EXECUTABLE)
